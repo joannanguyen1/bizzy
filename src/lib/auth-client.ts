c0a3868202c7fc8d628dcaedd7594d899d0e1a14
joannanguyen1 <joannanguyen569@gmail.com>
2025-10-31 14:50:14 -0400

    login/sign up pages linked to db

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,5 @@
+import { createAuthClient } from "better-auth/client";
+import { twoFactorClient } from "better-auth/client/plugins"; 
+export const authClient = createAuthClient({
+    plugins: [] 
+})
