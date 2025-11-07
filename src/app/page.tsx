1e91950dbd7e2879219c9a5d2e329f7f73d7d7d6
tony <tonydrayton43@gmail.com>
2025-11-06 20:05:42 -0500

    start the landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 import Map from "@/components/Map";
+import NavBar from "@/components/nav-bar";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen bg-zinc-50 dark:bg-black text-black dark:text-white flex flex-col items-center justify-center p-10">
-      <h1 className="text-3xl font-bold mb-6">Explore Fun Activities in Philadelphia</h1>
-      <Map />
-    </main>
+    <>
+      <main>
+        <section className="h-screen max-w-screen">
+          <NavBar />
+          <div className="mx-auto max-w-lg my-20 flex flex-col items-start">
+            <div className="font-light text-4xl mb-4">
+              <h1>Your circle.</h1>
+              <h1>Your city.</h1>
+              <h1>Your friends.</h1>
+            </div>
+            <Button className="rounded-full">Begin your hive</Button>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
